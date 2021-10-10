5149c221ce8d698e99c05bc9cdba99146f015181
GoodlookinAppa <leon.deeb@gmail.com>
2021-10-10 16:38:10 +0200

    Annie

---
+++ assets/preload/data/dad-battle/modchart.lua
@@ -0,0 +1,18 @@
+function start (song)
+        print("Succesfully initialized our song (dadbattle)")
+end
+
+function update (elapsed)
+        camHudAngle = camHudAngle + 1
+end
+function beatHit (beat)
+        print("Da beat has hit")
+end
+
+function stepHit (beat)
+
+end
+
+function keyPressed (key)
+
+end
